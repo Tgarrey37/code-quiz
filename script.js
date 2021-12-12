a674c63d4e2b07f15f545963a00b8fd9cfb7bd09
Tgarrey37 <tpopp0303@gmail.com>
2021-12-12 15:21:17 -0700

    Updated some of my files with my tutor

--- script.js
+++ script.js
@@ -4,27 +4,50 @@ var startQuizBtnEl = document.querySelector(".startquiz");
 var quizEl = document.querySelector("#quiz");
 var questionEl = document.querySelector("#question");
 var answersEl = document.querySelector("#answers");
+var welcomeContainer = document.querySelector(".container");
+var startDiv = document.querySelector(".startdiv");
 var currentQuestion = 0;
 var timeGiven = 100;
 var secondsElapsed = 0;
 var interval;
 
+// function startTimer() {
+//   timerEl.textContent = timeGiven;
+//   interval = setInterval(function () {
+//     secondsElapsed++;
+//     timerEl.textContent = timeGiven - secondsElapsed;
+//     if (secondsElapsed >= timeGiven) {
+//       currentQ = questions.length;
+//       nextQuestion();
+//     }
+//   }, 1000);
+// }
 function startTimer() {
   timerEl.textContent = timeGiven;
   interval = setInterval(function () {
-    secondsElapsed++;
-    timerEl.textContent = timeGiven - secondsElapsed;
-    if (secondsElapsed >= timeGiven) {
-      currentQ = questions.length;
-      nextQuestion();
+    timeGiven--;
+    timerEl.textContent = timeGiven;
+    if (timeGiven <= 0) {
+      clearInterval(interval);
+      timerEl.textContent = 0;
+      // execute end game function
+      endGame();
     }
   }, 1000);
 }
 startQuizBtnEl.addEventListener("click", function () {
   startTimer();
+  welcomeContainer.style.display = "none";
+  startDiv.style.display = "none";
+  // quizEl.style.display = "block";
+  quizEl.classList.remove("hide");
 });
 
 //stops timer
 function stopTimer() {
   clearInterval(interval);
+}
+
+function endGame() {
+  console.log("End game function triggered");
 }
