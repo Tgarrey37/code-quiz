820ecf31f7b16a8f144fd67fd5d4648c3a1d0027
Tgarrey37 <tpopp0303@gmail.com>
2021-12-13 09:56:56 -0700

    Changed file structure, added assets folder

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -41,6 +41,8 @@ startQuizBtnEl.addEventListener("click", function () {
   startDiv.style.display = "none";
   // quizEl.style.display = "block";
   quizEl.classList.remove("hide");
+  renderQuestion();
+  show(quizEl);
 });
 
 //stops timer
@@ -48,6 +50,48 @@ function stopTimer() {
   clearInterval(interval);
 }
 
-function endGame() {
-  console.log("End game function triggered");
+// function endGame() {
+//   console.log("End game function triggered");
+// }
+
+function renderQuestion() {
+  questionEl.textContent = questions[currentQuestion].title;
+  for (i = 0; i < answersEl.children.length; i++) {
+    answersEl.children[i].children[0].textContent = `${i + 1}: ${
+      questions[currentQuestion].choices[i]
+    }`;
+  }
 }
+
+function nextQuestion() {
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    renderQuestion();
+  } else {
+    stopTimer();
+    if (timeGiven - secondsElapsed > 0) score += timeGiven - secondsElapsed;
+    userScoreEl.textContent = score;
+    hide(quizEl);
+    show(inputScoreEl);
+    timerEl.textContent = 0;
+  }
+}
+function checkAnswer(answer) {
+  if (
+    questions[currentQuestion].answer ==
+    questions[currentQuestion].choices[answer.id]
+  ) {
+    score += 5;
+    displayMessage("Correct!");
+  } else {
+    timeGiven - 10;
+    displayMessage("Incorrect!");
+  }
+}
+
+answersEl.addEventListener("click", function (e) {
+  if (e.target.matches("button")) {
+    checkAnswer(e.target);
+    nextQuestion();
+  }
+});
